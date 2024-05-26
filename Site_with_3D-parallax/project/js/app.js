724624a98357c24eaba1d3c6e9b5e6e0d621ac29
Vlad Sushchenko <71527547+6Glow@users.noreply.github.com>
2024-05-26 13:50:59 +0300

    Update app.js
    added comments to the code

--- Site_with_3D-parallax/project/js/app.js
+++ Site_with_3D-parallax/project/js/app.js
@@ -1,4 +1,6 @@
+// Add an event listener for 'mousemove' to the document
 document.addEventListener('mousemove', e => {
+	 // Use Object.assign to modify the style of the root element
 	Object.assign(document.documentElement, {
 		style: `
 		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
